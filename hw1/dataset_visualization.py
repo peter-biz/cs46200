6c9cf10af1cd2eeee80464d96bec4be7c699ab7a
peter-biz <pgbizoukas@gmail.com>
2024-09-11 14:40:26 -0500

    stuff

--- hw1/dataset_visualization.py
+++ hw1/dataset_visualization.py
@@ -1,2 +1,9 @@
 # Problem 3
-
+'''You will download iris dataset from this site https://archive.ics.uci.edu/dataset/53/iris
+Using python convert .data files into .csv.
+Utilize matplotlib to select a graphical representation eg. Scatter plot, 
+box and whiskers [of your choice] that best explain the features of the data set.
+Write why you think that graphical representation is appropriate. 
+Upload your .csv, .py and all other files required to run the program.
+ You will be providing the evaluator with clear instructions on how to accept the input in a separate readme file with problem no.
+'''
--- hw1/diagonal_dif.py
+++ hw1/diagonal_dif.py
@@ -1,1 +1,29 @@
 # Problem 1
+'''Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+For example, the square matrix is shown below:
+1 2 3
+4 5 6
+9 8 9  
+
+The left-to-right diagonal 1 + 5+ 9 = 15 . The right to left diagonal 3 + 5+ 9= 17. 
+Their absolute difference is |15 – 7| = 2. 
+
+# Your program’s outcome should be an INTEGER |x| i.e. the absolute diagonal difference.
+# The matrix could have both positive and negative numbers. 
+# You will be using numpy to solve the problem.
+# Array size m x n must be less than 100 x 100. 
+
+The array should be based on user input. 
+You will be providing the evaluator with clear instructions on how to accept 
+the input in a separate readme file with problem no. 
+'''
+
+import numpy as np
+
+
+def diagonal_dif(arr):
+    return(array)
+
+array = input("Enter the matrix: ")
+print(diagonal_dif(array))
+input("Press Enter to exit")
