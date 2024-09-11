8987377159303857fa2f804f34b52a69db30006e
peter-biz <pgbizoukas@gmail.com>
2024-09-11 15:09:59 -0500

    idk what im doing at all

--- hw1/diagonal_dif.py
+++ hw1/diagonal_dif.py
@@ -6,7 +6,7 @@ For example, the square matrix is shown below:
 9 8 9  
 
 The left-to-right diagonal 1 + 5+ 9 = 15 . The right to left diagonal 3 + 5+ 9= 17. 
-Their absolute difference is |15 – 7| = 2. 
+Their absolute difference is |15 – 17| = 2. 
 
 # Your program’s outcome should be an INTEGER |x| i.e. the absolute diagonal difference.
 # The matrix could have both positive and negative numbers. 
@@ -21,9 +21,8 @@ the input in a separate readme file with problem no.
 import numpy as np
 
 
-def diagonal_dif(arr):
-    return(array)
-
+    
+#Input must be in this format: [[1,2,3],[4,5,6],[9,8,9]]
 array = input("Enter the matrix: ")
 print(diagonal_dif(array))
 input("Press Enter to exit")
