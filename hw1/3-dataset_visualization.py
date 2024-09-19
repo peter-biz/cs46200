b389d335136e5a49ed3d0aaa6f10d2722c0574d6
peter-biz <pgbizoukas@gmail.com>
2024-09-19 13:34:24 -0500

    messed around w/ #3, have a very rough/test plot. check #2 at home

--- hw1/3-dataset_visualization.py
+++ hw1/3-dataset_visualization.py
@@ -8,14 +8,15 @@ Upload your .csv, .py and all other files required to run the program.
  You will be providing the evaluator with clear instructions on how to accept the input in a separate readme file with problem no.
 '''
 
-import matplotlib as mp
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import csv
 
 # Conver iris.data to a .csv
-with open('iris.data', 'r') as in_file: # Open data.iris with 'r' parameter(read)
+with open('.\iris.data', 'r') as in_file: # Open data.iris with 'r' parameter(read)
     read = csv.reader(in_file)
-    with open('iris.csv', 'w') as out_file:
+    with open('.\iris.csv', 'w') as out_file:
         write = csv.writer(out_file)
         # Set column names
         write.writerow(('Septal Length', 'Septal Width', 'Petal Length', 'Petal Width', 'Species'))
@@ -25,3 +26,24 @@ with open('iris.data', 'r') as in_file: # Open data.iris with 'r' parameter(read
 # Utilize matplotlib to select a graphical representation eg. scatter plot, box and whsikers, {chose}, that best explain the features of the data set
 # write why you think that graphical represntation is approriate
 # upload all your .csv, .py and all other files required to run program
+
+plt.style.use('_mpl-gallery')
+
+# Read csv data
+iris = pd.read_csv('.\iris.csv')
+
+
+# This is just a test plot, i want to make it a scatter plot of petal lengths with differnt colored dots representing the species
+plt.figure(figsize=(10,6))
+plt.scatter(iris['Petal Length'], iris['Species'], marker='o', linestyle='-', color='b', label='Species')
+plt.title('Species by Petal Length')
+plt.xlabel('Petal Length')
+plt.ylabel('Species')
+plt.xticks(rotation=45)
+plt.legend()
+plt.grid()
+
+# Show the plot
+plt.tight_layout()
+plt.show()
+
