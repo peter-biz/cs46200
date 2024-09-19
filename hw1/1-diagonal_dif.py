060b88056011ed4309f49789fa8dfc82d7389bc4
peter-biz <pgbizoukas@gmail.com>
2024-09-19 11:21:57 -0500

    refactored problem 1 and added comments/err check

--- hw1/1-diagonal_dif.py
+++ hw1/1-diagonal_dif.py
@@ -20,35 +20,46 @@ the input in a separate readme file with problem no.
 #TODO: add comments and try-catch/error checking + write readme
 import numpy as np
 
-def num_rows():
-    rows = input("Input number of rows & columns: ")
-    print(rows)
-    return int(rows)
-
 def matrix_diag_abs_dif():
-    matrix = []
-    rows = num_rows()
-    print("Enter rows with numbers seperated by spaces")
-    for i in range(rows):
-        row_input = input("Enter row " + str(i+1) + ": ").split(" ")
-        matrix.append([int(element) for element in row_input])
+    matrix = [] # Initialize matrix
 
-    print("Entered matrix: " + str(matrix))
-    left_diag = np.array(matrix)
+    try:
+        rows = input("Input number of rows & columns: ") # Number of rows & columns, this also ensures it's square
+        rows = int(rows) # Converts string to int
+        if rows > 100:
+            print("Error: Matrix size should be less than 100x100.")
+            matrix_diag_abs_dif() # If matrix size is wrong, calls function again
 
-    # Need to flip the matrix around to get the right diagonal
-    flipped_matrix = np.flip(matrix, axis=1)
-    right_diag = np.array(flipped_matrix)
+        print("!!!Enter rows with numbers seperated by spaces. Ex: 'Enter row 1: 1 2 3'!!!") # Input instructions
+        for i in range(rows):
+            row_input = input("Enter row " + str(i+1) + ": ").split(" ") # Removes the spaces from the string
+            if len(row_input) != rows: # Checks if the input is enterered incorrectly
+                raise ValueError(f"Error: row does not have correct number of elements or has an invalid input.\n Ex: 'Enter row 1: 1 2 3'.")            
+            else:
+                matrix.append([int(element) for element in row_input]) # Adds elements of row_input to the matrix
 
-    print("Left Diagonal: " + str(diagonal_dif(left_diag)))
-    print("Right Diagonal: " + str(diagonal_dif(right_diag)))
+        print("Entered matrix: " + str(matrix))
+        left_diag = np.array(matrix) # Top left to bottom right diagonal 
 
-    abs_dif = abs(diagonal_dif(left_diag) - diagonal_dif(right_diag))
-    return abs_dif
+        # Need to flip the matrix around to get the right diagonal
+        flipped_matrix = np.flip(matrix, axis=1)
+        right_diag = np.array(flipped_matrix) # Top right to bottom left diagonal
 
-def diagonal_dif(a):
+        print("Left Diagonal: " + str(diagonal_dif(left_diag)))
+        print("Right Diagonal: " + str(diagonal_dif(right_diag)))
+
+        abs_dif = abs(diagonal_dif(left_diag) - diagonal_dif(right_diag)) # Absolute difference
+        print("Absoulte Difference: " + str(abs_dif))
+        input("Press enter to exit.")
+    except ValueError as e: # Error check
+        print(e)
+        matrix_diag_abs_dif()
+
+def diagonal_dif(a): # Gets the diagonal of a given array
     return np.trace(a)
-    
-#Input must be in this format: [[1,2,3],[4,5,6],[9,8,9]]
-print("Absoulte Difference: " + str(matrix_diag_abs_dif()))
-input("Press Enter to exit")
+
+def main(): # main
+    matrix_diag_abs_dif() # First call of function to start the program
+
+if __name__=="__main__":
+    main() # Calls main
