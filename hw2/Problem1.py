97a430e79ca148a88b5dfc717c6c1672037cfc46
peter-biz <pgbizoukas@gmail.com>
2024-09-24 09:53:32 -0500

    init2

--- hw2/Problem1.py
+++ hw2/Problem1.py
@@ -1,4 +1,4 @@
-#Peter Bizoukas, Python 3.8
+# Problem #1 - Peter Bizoukas, Python 3.8
 
 # 1.	Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, 
 # return the k closest points to the origin (0, 0).
--- hw2/Problem2.py
+++ hw2/Problem2.py
@@ -1,1 +1,7 @@
-# Peter Bizoukas, Python 3.8
+# Problem #2 - Peter Bizoukas, Python 3.8
+# 2.	Using the iris data set of assignment 1 [https://archive.ics.uci.edu/dataset/53/iris,
+# using python convert .data files into .csv.] split it into 
+# a.	80% train and 20% test data
+# b.	70% train and 30% test data 
+# c.	Compare the accuracy, specificity and sensitivity of a and b and with the ROC curve 
+# mention which is a better model. The grader should be able to generate the curve and the stats after running your program. 
