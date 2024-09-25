d5f87ea89fa8bfe1e8961b4bd909e3ea8895f812
peter-biz <pgbizoukas@gmail.com>
2024-09-25 12:52:28 -0500

    p1, setup point inputs and k input

--- hw2/Problem1.py
+++ hw2/Problem1.py
@@ -18,13 +18,30 @@
 # Explanation: The answer [[-2,4],[3,3]] would also be accepted.
 #  These are examples. The problem is user input based. The grader can test with any input of their choice. 
 
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
 
-def closest_points():
-    print("hi")
+
+
+def closest_points(pts, k):
+    print(pts, k)
 
 
 def main():
-    closest_points()
+    points = []
+    try:
+        print("Enter points in the format 'x y', with commas seperating each point. Ex: '1 3, -2 2'")
+        pts = input("Input points: ")
+        pts = pts.split(", ")
+        for i in pts:
+            points.append(i.split(" "))
+        print(points)
+        k = int(input("Input k: "))
+        closest_points(points, k)
+    except ValueError as e:
+        print(e)
+        main()
 
 if __name__=="__main__":
     main()
