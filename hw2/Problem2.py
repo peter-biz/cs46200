7f8e7c65e0564bc43b2f21e020f1d39b0495183b
peter-biz <pgbizoukas@gmail.com>
2024-09-25 20:35:04 -0500

    p2

--- hw2/Problem2.py
+++ hw2/Problem2.py
@@ -5,3 +5,20 @@
 # b.	70% train and 30% test data 
 # c.	Compare the accuracy, specificity and sensitivity of a and b and with the ROC curve 
 # mention which is a better model. The grader should be able to generate the curve and the stats after running your program. 
+
+
+import numpy as np
+import scipy as sp
+import matplotlib as plt
+
+
+# Conver iris.data to a .csv
+with open('.\iris.data', 'r') as in_file: # Open data.iris with 'r' parameter(read)
+    read = csv.reader(in_file)
+    with open('.\iris.csv', 'w') as out_file:
+        write = csv.writer(out_file)
+        # Set column names
+        write.writerow(('Septal Length', 'Septal Width', 'Petal Length', 'Petal Width', 'Species'))
+        for row in read:
+            write.writerow(row)
+
