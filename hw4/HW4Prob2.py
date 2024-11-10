4623343e21a825f5313a737612e02a2e340662b1
peter-biz <pgbizoukas@gmail.com>
2024-11-10 16:46:10 -0600

    working hw4prob2

--- hw4/HW4Prob2.py
+++ hw4/HW4Prob2.py
@@ -10,9 +10,59 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-
 def main():
+    # Load the data
+    data = pd.read_csv('hw4\housing.csv')
+    data = data.dropna()
+    data = data.drop(['ocean_proximity'], axis=1)
 
+    # Normalize the data
+    data = (data - data.mean()) / data.std()
+
+    # Split the data into training and testing
+    X = data.drop(['median_house_value'], axis=1)
+    y = data['median_house_value']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0) # 80-20 split, random_state for reproducibility
+
+    # Add bias to the input
+    X_train['bias'] = 1
+    X_test['bias'] = 1
+
+    # Initialize the weights
+    np.random.seed(0) # For reproducibility
+    weights = np.random.rand(X_train.shape[1])
+
+    # Store the initial, middle and final weights
+    init_weights = weights.copy()
+
+    # Train the model
+    learning_rate = 0.01
+    for i in range(1000):
+        # Forward pass
+        y_pred = np.dot(X_train, weights) # Linear regression
+        loss = np.mean((y_pred - y_train) ** 2) # MSE
+
+        # Backward pass
+        gradient = np.dot(X_train.T, y_pred - y_train) / X_train.shape[0]
+        weights -= learning_rate * gradient # Update weights
+
+        print(f'Iteration {i}: Loss: {loss}, Weights: {weights}')
+
+        if(i == 500): # Store the middle weights
+            mid_weights = weights.copy()
+
+    final_weights = weights 
+
+    # Test the model
+    y_pred = np.dot(X_test, weights)
+    loss = np.mean((y_pred - y_test) ** 2)
+    print(f'Test Loss: {loss}')
+
+    # Print intial, middle and final weights
+    print("Backpropagation Weight Updates: ")
+    print(f'Initial Weights: {init_weights}')
+    print(f'Middle Weights: {mid_weights}')
+    print(f'Final Weights: {final_weights}')
 
 if __name__ == "__main__":
     main()
---
+++ hw4/README-2.md
@@ -0,0 +1,9 @@
+# Problem 2 - HW4 - Backpropagation
+There is no required input for this .py file. Run it(it must be in the same folder that cmd is running from) and it will output the weights and loss of each iteration. It displays a summary at the end
+of the inital weights, middle weights, and final weights. It also displays the final loss.
+
+## Required packages:
+* matplotlib
+* numpy
+* pandas
+* sklearn
